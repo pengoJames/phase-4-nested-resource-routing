7678d49c8cd456e642dfc80aad413bc4fc2c5055
Pe-ngo <jamespengo95@gmail.com>
2022-10-08 12:36:03 +0300

    Completed Assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,14 @@ class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    reviews = Review.all
+    # allow us to get the index of all reviews for a certain dog house
+    if params [:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews  
+    else
+      # allow access to the index of all reviews
+      reviews = Review.all
+    end 
     render json: reviews, include: :dog_house
   end
 
